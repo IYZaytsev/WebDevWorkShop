756fb79a47d881a257af75223939265960e90662
Ivan <IvanZaytsev123@gmail.com>
2019-08-24 12:00:55 -0400

    added js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-var x = prompt('type any number: ');
-var resp = x*5;
-console.log("your answer times 5 is: " + resp);
+function getPeople(){
+    const ul = document.createElement('ul');
+
+    fetch('https://swapi.co/api/people')
+    .then((resp) => resp.json())
+    .then((people) =>{
+        console.log("this is people " + people);
+        people.results.forEach((person) => {
+            console.log("this is person" + person);
+            const li = document.createElement('li');
+            li.innerHTML = `<a 
+                href = "https://en.wikipedia.org/wiki/${person.name}"
+                target = "_blank">${person.name}</a>`;
+        ul.appendChild(li);
+        });
+
+        document.body.appendChild(ul);
+    });
+}
